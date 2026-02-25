a4e2ad21996018c75135c3badb8eefc514120d62
Omkar2499 <omkarpatil2448@gmail.com>
2026-02-25 11:37:22 +0530

    Changes to Python File

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,14 @@
-print("Hello world")
+arr=[1,2,4,7,99]
+
+
+def maximum(arr):
+    for i in range(0,len(arr)):
+        maxi=arr[0]
+        if maxi>arr[i]:
+            maxi=arr[i]
+        
+        return maxi
+    
+
+print(maximum(arr))
+
